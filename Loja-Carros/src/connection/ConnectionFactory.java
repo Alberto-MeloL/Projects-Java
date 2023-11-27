a994f72715a9c47a0eac0ea3e795ad323ccfbe73
Dev_Alberto <albertomeloteco@gmail.com>
2023-11-27 00:22:52 -0300

    27/11/2023

--- Loja-Carros/src/connection/ConnectionFactory.java
+++ Loja-Carros/src/connection/ConnectionFactory.java
@@ -12,7 +12,7 @@ public class ConnectionFactory {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "postgres";
+    private static final String SENHA = "postgres1";
 
     /* Métodos */
 
